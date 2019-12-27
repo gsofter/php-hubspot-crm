aa8dbf1d107e8bd1bb11f279c875e73ff340535d
unknown <guru.softwaremaster@mail.ru>
2019-12-27 14:41:51 +0400

    UPDATE: second task

--- api.php
+++ api.php
@@ -33,6 +33,53 @@ class ContactAPI {
 			'data' 	 => $response,
 		];
 	}
+
+	/**
+	* Create or update a group of contacts
+	*
+	* @param array $array
+	* @return status
+	*/
+	public static function UpdateContactProperties($array) {
+		$hapikey = '9f487cb8-c9fc-4a4a-8b6a-2a2f482261e7';
+		$endpoint = "https://api.hubapi.com/contacts/v1/contact/batch/?hapikey=" . $hapikey;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $endpoint);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($array));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		@curl_close($ch);
+		return [
+			'status' => $status_code,
+		];
+	}
+
+	/**
+	* Update an existing contact
+	*
+	* @param array $array
+	* @param int $vid
+	* @return status
+	*/
+	public static function UpdateContact($vid, $array) {
+		$hapikey = '9f487cb8-c9fc-4a4a-8b6a-2a2f482261e7';
+		$endpoint = "https://api.hubapi.com/contacts/v1/contact/vid/101/profile?hapikey=" . $hapikey;
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $endpoint);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+		curl_setopt($ch, CURLOPT_POST, true);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($array));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		$response = curl_exec($ch);
+		$status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+		@curl_close($ch);
+		return [
+			'status' => $status_code,
+		];
+	}
 }
 
 class CrmAPI {
